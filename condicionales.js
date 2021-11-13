392b185d86ae2fc674f1bba6fabea906df0588cf
chatemar <41093360+chatemar@users.noreply.github.com>
2021-11-12 23:10:42 -0600

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,26 @@
+//alert ("Hola desde javascript");
+let edad=24;
+	if (edad >= 18) {
+		console.log("Mayor de edad");
+		console.log("Ya eres un adulto");
+	}
+	else {
+		console.log("Menor de edad");
+		console.log("eres un niño");
+	}
+
+let precio=200;
+	if (precio>100){
+		console.log("Descuento" , precio-10);
+	}
+
+let A=10;
+let B=20;
+if (A-B) 
+{
+	console.log("true x");
+}
+else 
+{
+	console.log("false");
+}
